e7054a7dcd20e158fa66b89c79a4dd246eb6098a
王鹏飞 <todayisall@qq.com>
2023-01-11 22:06:23 +0800

    feat: 初步完成功能.

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -3,9 +3,27 @@ pub mod domain;
 pub mod service;
 pub mod web;
 
+pub use data::DataError;
 pub use domain::clip::field::ShortCode;
 pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
-pub use data::DataError;
-
 pub use service::ServiceError;
+
+use data::AppDatabase;
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
+use web::renderer::Renderer;
+
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+    rocket::build()
+        .manage::<AppDatabase>(config.database)
+        .manage::<Renderer>(config.renderer)
+        .mount("/", web::http::routes())
+        .mount("/static", FileServer::from("static"))
+        .register("/", web::http::catcher::catchers())
+}
+
+pub struct RocketConfig {
+    pub renderer: Renderer<'static>,
+    pub database: AppDatabase,
+}
